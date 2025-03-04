872915bf88a93f0ca3b723e63be227635bb2fa6e
Jay Kazuma <gintamashika@gmail.com>
2025-03-04 09:57:15 +0800

    responsive home

--- src/app/hero/hero.tsx
+++ src/app/hero/hero.tsx
@@ -9,7 +9,7 @@ const Hero = () => {
 
     return (
         <>
-        <Head>
+            <Head>
                 <title>Millenium Toolings and Fabrication Inc.</title>
                 <meta name="description" content="Millenium Toolings provides top-quality engineering tools for professionals." />
                 <meta name="keywords" content="Millenium Toolings, engineering tools, fabrication, supplier, manufacturing, millenium toolings and fabrication, millenium location, millenium website, millenium.com" />
@@ -21,49 +21,48 @@ const Hero = () => {
                 <link rel="icon" href="/favicon.ico" />
                 <meta property="og:url" content="https://milleniumtoolings.vercel.app" />
             </Head>
-        <section className="relative flex flex-col min-h-screen bg-[url('/Topview.png')] bg-cover bg-center bg-no-repeat">
-            <div className="relative flex-grow bg-[url('/HEXAGON.png')] bg-contain bg-center bg-no-repeat">
-                {!loaded && <Loading onLoaded={() => setLoaded(true)} />}
-                {loaded && (
-                    <>
-                        
-                        <div className="absolute inset-0 bg-black/60"></div>
+            <section className="relative flex flex-col min-h-screen bg-[url('/Topview.png')] bg-cover bg-center bg-no-repeat w-full overflow-hidden">
+                <div className="relative flex-grow bg-[url('/HEXAGON.png')] bg-contain bg-center bg-no-repeat w-full">
+                    {!loaded && <Loading onLoaded={() => setLoaded(true)} />}
+                    {loaded && (
+                        <>
+                            <div className="absolute inset-0 bg-black/60"></div>
 
-                        
-                        <div className="relative mx-auto max-w-screen-xl px-6 py-32 sm:px-12 flex flex-col lg:justify-center lg:flex-grow lg:px-16">
-                            <div className="max-w-2xl text-center sm:text-left border border-gray-700 rounded-lg p-6 bg-black/40">
-                                <h1 className="text-6xl font-extrabold text-white sm:text-7xl leading-tight">
-                                    <span className="bg-gradient-to-r from-green-400 via-blue-500 to-red-500 bg-clip-text text-transparent">
-                                        MILLENIUM
-                                    </span>
-                                </h1>
-                                <h2 className="text-3xl font-semibold text-gray-300 mt-2">
-                                    Toolings & Fabrication Inc.
-                                </h2>
-                                <div className="pt-5">
-                                    <h3 className="text-2xl font-semibold text-white text-center">OUR VISION</h3>
-                                    <p className="mt-2 text-white">
-                                        To be recognized as most preferred SUPPLIER / PARTNER of our valued
-                                        customers in delivering our whole range of products and services beyond our
-                                        capacity.
-                                    </p>
+                            <div className="relative mx-auto w-full px-6 py-32 sm:px-12 flex flex-col lg:justify-center lg:flex-grow lg:px-16">
+                                <div className="max-w-2xl text-center sm:text-left border border-gray-700 rounded-lg p-6 bg-black/40">
+                                    <h1 className="text-5xl sm:text-6xl font-extrabold text-white leading-tight text-center sm:text-left break-words w-full">
+                                        <span className="bg-gradient-to-r from-green-400 via-blue-500 to-red-500 bg-clip-text text-transparent inline-block w-full">
+                                            MILLENIUM
+                                        </span>
+                                    </h1>
 
-                                    <h3 className="text-2xl font-semibold text-white text-center">OUR MISSION</h3>
-                                    <p className="mt-2 text-white">
-                                        To satisfy our valued customer by delivering world class quality products
-                                        and services at the most reasonable cost, in the most appropriate time
-                                        beyond their expectations through reinforced partnership with our own
-                                        suppliers and our most valuable resources - our manpower / personnel ,
-                                        providing a smooth and stimulating working environment, rewarding benefits
-                                        coupled with dignity.
-                                    </p>
+                                    <h2 className="text-3xl font-semibold text-gray-300 mt-2">
+                                        Toolings & Fabrication Inc.
+                                    </h2>
+                                    <div className="pt-5">
+                                        <h3 className="text-2xl font-semibold text-white text-center">OUR VISION</h3>
+                                        <p className="mt-2 text-white">
+                                            To be recognized as most preferred SUPPLIER / PARTNER of our valued
+                                            customers in delivering our whole range of products and services beyond our
+                                            capacity.
+                                        </p>
+
+                                        <h3 className="text-2xl font-semibold text-white text-center">OUR MISSION</h3>
+                                        <p className="mt-2 text-white">
+                                            To satisfy our valued customer by delivering world class quality products
+                                            and services at the most reasonable cost, in the most appropriate time
+                                            beyond their expectations through reinforced partnership with our own
+                                            suppliers and our most valuable resources - our manpower / personnel,
+                                            providing a smooth and stimulating working environment, rewarding benefits
+                                            coupled with dignity.
+                                        </p>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </>
-                )}
-            </div>
-        </section>
+                        </>
+                    )}
+                </div>
+            </section>
         </>
     );
 };
