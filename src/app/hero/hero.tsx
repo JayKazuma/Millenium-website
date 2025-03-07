d7e92afb413a326c35d67095722e859e62d12660
Jay Kazuma <gintamashika@gmail.com>
2025-03-07 11:09:32 +0800

    Analytics installed

--- src/app/hero/hero.tsx
+++ src/app/hero/hero.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Analytics } from "@vercel/analytics/react"
 import Head from "next/head";
 import React, { useState } from "react";
 import Loading from "../loading/loading";
